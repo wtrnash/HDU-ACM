0e5f2144e5b9b755fdde1283636a4895454cdf72
wtr <396612789@qq.com>
2017-11-08 17:07:52 +0800

    第五章动态规划

--- 第五章动态规划/Max Sum.cpp
+++ 第五章动态规划/Max Sum.cpp
@@ -20,5 +20,56 @@ Case 1:
 Case 2:
 7 1 6
 ****************************************************************************************************************************************************/
+//最大子序列和，可以用一种思路就是用sum记录和，只有之前的和是正的，作用在后面才是有效的，否则sum就从0开始重新开始记录一个子序列。 
+#include <iostream>
+#include <vector>
+using namespace std;
 
-
+int main()
+{
+	int t, n, num;
+	vector<int> v;
+	cin >> t;
+	for(int i = 0;i < t; i++)
+	{
+		v.clear();
+		cin >> n;
+		while(n--)
+		{
+			cin >> num;
+			v.push_back(num);
+		}
+		
+		int max = -1001;
+		int start, end ;
+		int tempStart = 0;
+		int sum = 0;
+		
+		for(int j = 0; j < v.size(); j++)
+		{
+			sum += v[j];
+			if(sum > max)				//sum比max大，则更新max 
+			{
+				start = tempStart + 1;
+				end = j + 1;
+				max = sum;
+			}
+			
+			if(sum < 0)					//当前的sum已经为负了，加上后面的起副作用，所以重新开始记一个子序列。 
+			{
+				sum = 0;
+				if(j + 1 < v.size())
+					tempStart = j + 1;
+			}
+			
+		
+		}
+		
+		cout << "Case " << i + 1 << ":" << endl;
+		cout << max << " " << start << " " << end << endl;
+		if(i < t - 1)
+			cout << endl; 
+		
+	}
+	return 0;
+}
