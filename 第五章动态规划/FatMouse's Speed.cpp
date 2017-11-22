2bd2c439bdf4151d0c46fabf74d4c5104d4a36ad
wtr <396612789@qq.com>
2017-11-22 17:06:05 +0800

    第五章动态规划

--- 第五章动态规划/FatMouse's Speed.cpp
+++ 第五章动态规划/FatMouse's Speed.cpp
@@ -35,6 +35,9 @@ Sample Output
 7
 ********************************************************************************************************************************************************/
 //二维类似最长子序列的动态规划 
+//将体重按升序排序，用vector<vector<int> >存储每个序列的序号 ，其中元素size最长的即是最长序列
+//从第一只老鼠开始，第一只老鼠的序列只有一个元素。第二只老鼠和之前的老鼠比，取 之前的老鼠体重小且速度快的 且 序列最长的，将序列复制到当前序列，并加上本身
+//以此类推  
 #include <iostream>
 #include <vector>
 #include <algorithm> 
@@ -49,6 +52,8 @@ struct Mice
 
 bool compare(Mice m1, Mice m2)	//体重升序排序 
 {
+	if (m1.weight == m2.weight) 
+		return m1.speed > m2.speed;
 	return m1.weight < m2.weight;
 }
 
@@ -60,7 +65,7 @@ int main()
 	int weight, speed;
 
 	int index = 1;
-	while (cin >> weight >> speed)
+	while (cin >> weight >> speed)	//读取数据 
 	{
 		temp.weight = weight;
 		temp.speed = speed;
@@ -68,23 +73,23 @@ int main()
 		v.push_back(temp);
 	}
 
-	sort(v.begin(), v.end(), compare);
+	sort(v.begin(), v.end(), compare);	//排序 
 
-	vector<int> v1;
+	vector<int> v1;					//第一个老鼠只含本身 
 	v1.push_back(v[0].index);
 	count.push_back(v1);
 
 	vector<int> vTemp;
-	for (int i = 1; i < v.size(); i++)
+	for (int i = 1; i < v.size(); i++)	//第二个老鼠开始 
 	{
 		vTemp.clear();
 		int length = 0;
 		int index;
-		for (int j = 0; j < i; j++)
+		for (int j = 0; j < i; j++)   //对之前所有的老鼠进行比较 
 		{
-			if (v[i].weight > v[j].weight && v[i].speed < v[j].speed)
+			if (v[i].weight > v[j].weight && v[i].speed < v[j].speed)		//如果比之前的重而且比之前的慢 
 			{
-				if (count[j].size() > length)
+				if (count[j].size() > length)                   //且当前的序列比之前最长的长，则选择当前的序列   
 				{
 					length = count[j].size();
 					index = j;
@@ -92,12 +97,12 @@ int main()
 			}
 		}
 
-		if (length == 0)
+		if (length == 0)					//如果长度为0，代表没有找到之前合适的老鼠 
 		{
-			vTemp.push_back(v[i].index);
+			vTemp.push_back(v[i].index);		//将本身加入vector 
 			count.push_back(vTemp);
 		}
-		else
+		else		//否则将之前最长的序列加上本身加入vector 
 		{
 			vTemp.assign(count[index].begin(), count[index].end());
 			vTemp.push_back(v[i].index);
@@ -106,18 +111,18 @@ int main()
 	}
 
 	int flag = 0;
-	for (int i = 1;i < count.size(); i++)
+	for (int i = 1;i < count.size(); i++)		//选择最长的 
 	{
 		if (count[i].size() > count[flag].size())
 			flag = i;
 	}
 
-	cout << count[flag].size() << endl;
-	for (int i = 0;i < vTemp.size(); i++)
+	cout << count[flag].size() << endl;			//输出 
+	for (int i = 0;i < count[flag].size(); i++)
 	{
 		cout << count[flag][i] << endl;
 	}
 
-
+	 
 	return 0;
 }
